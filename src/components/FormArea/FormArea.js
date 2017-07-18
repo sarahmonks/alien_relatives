89fb1802f652b34525a892fa50a6af5ffc61c375
Sarah Monks <monks.sarah@gmail.com>
2017-07-18 15:38:14 +0100

    updated the FormArea component to render different HTML depending on the state of the application

--- src/components/FormArea/FormArea.js
+++ src/components/FormArea/FormArea.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
-import './form_area.css';
 
+import './form_area.css';
+import OriginalForm from '../OriginalForm/OriginalForm';
+import AlienInformation from '../AlienInformation/AlienInformation';
 
 const FormArea = function(props) {
+		const originalFormIsDisplayed = props.originalFormIsDisplayed;
 
-	
+		let userInterativeArea = null;
+    	if (props.originalFormIsDisplayed) {
+      		userInterativeArea = <OriginalForm submitForm={props.submitForm} />;
+    	} else {
+      		userInterativeArea = <AlienInformation alienTribeName={props.alienTribeName} firstName={props.firstName} />;
+    	
+    	}
+
 		return (<div id="main_content_area">
 					<div className="background_color"></div>
 
 					<div id="user_interactive_area">
 						
-						<h2>What Alien tribe are you related to?</h2>
-						<form id="alien_tribe_form" name="alien_tribe_form" method="post" action="">
-			
-							<label>Please enter your first name:</label>
-							<input id="first_name" name="first_name" type="text" placeholder="Enter your first name" />
-							
-						</form>
-						<div id="submit_form" className="button_style" onClick={props.submitForm}>Submit</div>
-							
+						{userInterativeArea}
+						
 					</div>
 				</div>
 		);
