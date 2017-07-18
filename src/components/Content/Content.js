36707716ed7842dedfbeee538161e12b115b3721
Sarah Monks <monks.sarah@gmail.com>
2017-07-18 13:10:04 +0100

    added Content component

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Planet from '../Planet/Planet';
 import CupAndSaucer from '../CupAndSaucer/CupAndSaucer';
+import AlienTribeNameArea from '../AlienTribeNameArea/AlienTribeNameArea';
 import FormArea from '../FormArea/FormArea';
 import './content.css';
 
@@ -10,8 +11,10 @@ const Content = function(props) {
 	
 		return (<div id="content">
 			<Planet />
-			<CupAndSaucer />
-			<FormArea />
+			<CupAndSaucer alienTribeName={props.alienTribeName} alienTribeImage={props.alienTribeImage}
+					alienTribeImageHands={props.alienTribeImageHands} alienWisdomMessageMp3={props.alienWisdomMessageMp3}/>
+			<AlienTribeNameArea alienTribeName={props.alienTribeName}/>
+			<FormArea submitForm={props.submitForm}/>
 			</div>
 		);
  
