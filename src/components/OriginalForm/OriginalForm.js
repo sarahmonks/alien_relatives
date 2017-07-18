0aec4f6422e9303dc0d73f1c9a14e2d0387f1905
Sarah Monks <monks.sarah@gmail.com>
2017-07-18 15:36:45 +0100

    created OriginalForm component which contains the html form displayed to the user when the application is loaded first

---
+++ src/components/OriginalForm/OriginalForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import './original_form.css';
+
+
+const OriginalForm = function(props) {
+		return (<div>
+					<h2>What Alien tribe are you related to?</h2>
+					<form id="alien_tribe_form" name="alien_tribe_form" method="post" action="">
+			
+						<label>Please enter your first name:</label>
+						<input id="first_name" name="first_name" type="text" placeholder="Enter your first name" />
+							
+					</form>
+					<div id="submit_form" className="button_style" onClick={props.submitForm}>Submit</div>
+				</div>
+		);
+ 
+}
+
+export default OriginalForm;
