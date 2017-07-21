02ae5a23e9a746ffb89977f304e5ad68fe148a48
Sarah Monks <monks.sarah@gmail.com>
2017-07-21 18:11:00 +0100

    added another condition for rendering classes

--- src/components/CupAndSaucer/CupAndSaucer.js
+++ src/components/CupAndSaucer/CupAndSaucer.js
@@ -7,14 +7,20 @@ import saucer from '../../images/saucer.png';
 
 const CupAndSaucer = function(props) {
 		console.log(props.alienTribeImage);	
-		return (<div id="cup_and_saucer_container" className={(props.userChoseToMeetAlien === true ? 'rotate_cup_and_saucer_animation move_animation' : '')}>
+		return (<div id="cup_and_saucer_container" className={((props.userChoseToMeetAlien === true  && props.cupAndSaucerHaveArrived === false) ? 'rotate_cup_and_saucer_animation move_animation' : 'full_position')}>
 					<img src={cup_back} width="453" height="453" />
 					
-					<img id="alien_figure" className="alien_figure" src={props.alienTribeImage} width="453" height="453" />
-					<img className={"saucer_solo " + (props.userChoseToMeetAlien === true ? 'saucer_shake' : '')} src={saucer} width="453" height="453" />
+					<img id="alien_figure" className={"alien_figure "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
+							src={props.alienTribeImage} width="453" height="453" />
+					<img className={"saucer_solo " + ((props.userChoseToMeetAlien === true && props.cupAndSaucerHaveArrived === false) ? 'saucer_shake' : '')} 
+							src={saucer} width="453" height="453" />
 					<img src={cup_front} width="453" height="453" />
-					<img id="alien_figure_hands" className="alien_figure_hands" src={props.alienTribeImageHands} width="453" height="453" />
-					<span id="play_message" className="play_message button_style">Play Wisdom Message</span>
+					<img id="alien_figure_hands" className={"alien_figure_hands "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
+							src={props.alienTribeImageHands} width="453" height="453" />
+
+					<span id="play_message" className={"play_message button_style " + (props.cupAndSaucerHaveArrived === true ? 'show_play_button' : '')}>
+							Play Wisdom Message
+					</span>
 				</div>
 		);
  
