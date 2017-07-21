11fd91371312f98baf6246a0fe44b924ea7af579
Sarah Monks <monks.sarah@gmail.com>
2017-07-21 18:09:08 +0100

    tidied up code

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,26 +1,32 @@
 import React from 'react';
-
 import Planet from '../Planet/Planet';
 import CupAndSaucer from '../CupAndSaucer/CupAndSaucer';
 import AlienTribeNameArea from '../AlienTribeNameArea/AlienTribeNameArea';
 import FormArea from '../FormArea/FormArea';
 import './content.css';
 
-const Content = function(props) {
 
-	
-		return (<div id="content">
-			<Planet />
-			<CupAndSaucer alienTribeName={props.alienTribeName} alienTribeImage={props.alienTribeImage}
-					alienTribeImageHands={props.alienTribeImageHands} alienWisdomMessageMp3={props.alienWisdomMessageMp3} 
-					userChoseToMeetAlien={props.userChoseToMeetAlien}/>
-			<AlienTribeNameArea alienTribeName={props.alienTribeName}/>
-			<FormArea submitForm={props.submitForm} originalFormIsDisplayed={props.originalFormIsDisplayed} 
-			alienTribeName={props.alienTribeName} firstName={props.firstName} meetAlien={props.meetAlien} userChoseToMeetAlien={props.userChoseToMeetAlien}
-			resultMessageIsDisplayed={props.resultMessageIsDisplayed} />
-			</div>
+const Content = function(props) {
+		return (<div id="container">
+					<div id="content">
+						<Planet />
+						<CupAndSaucer alienTribeName={props.alienTribeName} 
+								alienTribeImage={props.alienTribeImage}
+								alienTribeImageHands={props.alienTribeImageHands} 
+								alienWisdomMessageMp3={props.alienWisdomMessageMp3} 
+								userChoseToMeetAlien={props.userChoseToMeetAlien} 
+								cupAndSaucerHaveArrived={props.cupAndSaucerHaveArrived} />
+						<AlienTribeNameArea alienTribeName={props.alienTribeName}/>
+						<FormArea submitForm={props.submitForm} 
+								originalFormIsDisplayed={props.originalFormIsDisplayed} 
+								alienTribeName={props.alienTribeName} 
+								firstName={props.firstName} 
+								meetAlien={props.meetAlien} 
+								userChoseToMeetAlien={props.userChoseToMeetAlien}
+								resultMessageIsDisplayed={props.resultMessageIsDisplayed} />
+					</div>
+				</div>
 		);
- 
 }
 
 export default Content;
