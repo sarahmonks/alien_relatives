c6df066e1826c1329ef905cc4ad03fed46fca27e
Sarah Monks <monks.sarah@gmail.com>
2017-07-21 19:57:21 +0100

    added conditional rendering to render text depending on the value of cupAndSaucerHaveArrived boolean property

--- src/components/ResultMessage/ResultMessage.js
+++ src/components/ResultMessage/ResultMessage.js
@@ -5,9 +5,21 @@ import './result_message.css';
 const ResultMessage = function(props) {
 		//This is a stateless component used in the AlienInformation component
 		//This component has a property called userChoseToMeetAlien which is a state defined in the main AppContainer class.
+
+
+		const cupAndSaucerHaveArrived = props.cupAndSaucerHaveArrived;
+
+		let output = null;
+    	if (cupAndSaucerHaveArrived) {
+    		output = 'He has a wisdom message for you';
+
+    	} else {
+      		output = (props.userChoseToMeetAlien === true ? 'Here comes the flying saucer' : 'Awwwwww no');
+    	}
+
 		return (<div id='result_message_area'>
 						<p className='large_text'>
-							{(props.userChoseToMeetAlien === true ? 'Here comes the flying saucer' : 'Awwwwww no')}!!!
+							{output}!!!
 						</p>
 				</div>
 		);
