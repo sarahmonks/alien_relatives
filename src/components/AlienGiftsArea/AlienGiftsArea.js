5ff04372e8e5385618d5cda347d5b967f35a1253
Sarah Monks <monks.sarah@gmail.com>
2017-07-22 14:36:11 +0100

    added new component giving user the option to give alien gifts

--- src/components/AlienGiftsArea/AlienGiftsArea.js
+++ src/components/AlienGiftsArea/AlienGiftsArea.js
@@ -7,15 +7,16 @@ const AlienGiftsArea = function(props) {
 		//To this component, we pass in a property called meetAlien (which is a method defined in the main AppContainer class).
 		return (<div id="alien_gifts_area" className={(props.giftAreaIsDisplayed === true ? 'show': '')}>
 					<div className="background_color"></div>
-					<div className="gifts_area">
+
+					<div className="gifts_area_content">
 						<p className="large_text">Give some love?</p>
 
-						<span id="gift_love" className="button_style main_button_style">
+						<span id="gift_love" onClick={props.giveLove.bind(null, true)} className="button_style main_button_style">
 							<i className="fa fa-heart"></i>
 							<span id="qty_of_love"></span>
 						</span>
 
-						<span id="no_thanks" className="large_text">No Thanks</span>
+						<span id="no_thanks" onClick={props.giveLove.bind(null, false)} className="large_text">No Thanks</span>
 						<span id="thank_you" className='large_text pink_text'>Thank you!</span>
 
 					</div>
