8d4d3afebc8d1179493692671e761991a6b07da8
Sarah Monks <monks.sarah@gmail.com>
2017-07-22 12:45:44 +0100

    added new class to buttons

--- src/components/MeetAlienButtons/MeetAlienButtons.js
+++ src/components/MeetAlienButtons/MeetAlienButtons.js
@@ -6,8 +6,8 @@ const MeetAlienButtons = function(props) {
 		//This is a stateless component used in the AlienInformation component
 		//To this component, we pass in a property called meetAlien (which is a method defined in the main AppContainer class).
 		return (<div id='result_message_area'>
-						<span id='meet_alien_button_yes' onClick={props.meetAlien.bind(null, true)} className='button_style'>Yes</span>
-						<span id='meet_alien_button_no'  onClick={props.meetAlien.bind(null, false)} className='button_style'>No</span>
+						<span id='meet_alien_button_yes' onClick={props.meetAlien.bind(null, true)} className='button_style main_button_style'>Yes</span>
+						<span id='meet_alien_button_no'  onClick={props.meetAlien.bind(null, false)} className='button_style main_button_style'>No</span>
 				</div>
 		);
  
