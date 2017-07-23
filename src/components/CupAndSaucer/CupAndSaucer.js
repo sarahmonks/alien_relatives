bdf6cff34dcae04d2a3d6b0cdfb290721c4ec50c
Sarah Monks <monks.sarah@gmail.com>
2017-07-23 14:57:21 +0100

    added the classnames module for complex conditionals

--- src/components/CupAndSaucer/CupAndSaucer.js
+++ src/components/CupAndSaucer/CupAndSaucer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import cn from 'classnames';
 
 import './cup_and_saucer.css';
 import cup_back from '../../images/cup-back.png';
@@ -12,23 +13,27 @@ const CupAndSaucer = function(props) {
 		//In this component, we will use the states userChoseToMeetAlien and cupAndSaucerHaveArrived (defined in the AppContainer class) 
 		//to determine which CSS classes should be output within the img tags
 
-		console.log(props.alienTribeImage);	
-		return (<div id="cup_and_saucer_container" className={((props.userChoseToMeetAlien === true  && props.cupAndSaucerHaveArrived === false) ? 'rotate_cup_and_saucer_animation move_animation' : 'full_position')}>
-					<img src={cup_back} width="453" height="453" />
+ 	const className = cn('some-default-class', {
+      'rotate_cup_and_saucer_animation move_animation': (props.userChoseToMeetAlien === true  && props.cupAndSaucerHaveArrived === false),
+      'full_position': (props.userChoseToMeetAlien === true  && props.cupAndSaucerHaveArrived === true)
+    });
+
+	return (<div id="cup_and_saucer_container" className={className}>
+				<img src={cup_back} width="453" height="453" />
 					
-					<img id="alien_figure" className={"alien_figure "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
+				<img id="alien_figure" className={"alien_figure "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
 							src={props.alienTribeImage} width="453" height="453" />
-					<img className={"saucer_solo " + ((props.userChoseToMeetAlien === true && props.cupAndSaucerHaveArrived === false) ? 'saucer_shake' : '')} 
+				<img className={"saucer_solo " + ((props.userChoseToMeetAlien === true && props.cupAndSaucerHaveArrived === false) ? 'saucer_shake' : '')} 
 							src={saucer} width="453" height="453" />
-					<img src={cup_front} width="453" height="453" />
-					<img id="alien_figure_hands" className={"alien_figure_hands "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
+				<img src={cup_front} width="453" height="453" />
+				<img id="alien_figure_hands" className={"alien_figure_hands "  + (props.cupAndSaucerHaveArrived === true ? 'show_alien' : '')} 
 							src={props.alienTribeImageHands} width="453" height="453" />
 
-					<span id="play_message" onClick={props.playAlienMessage} className={"play_message button_style " + (props.cupAndSaucerHaveArrived === true ? 'show_play_button' : '')}>
+				<span id="play_message" onClick={props.playAlienMessage} className={"play_message button_style " + (props.cupAndSaucerHaveArrived === true ? 'show_play_button' : '')}>
 							Play Wisdom Message
-					</span>
-				</div>
-		);
+				</span>
+			</div>
+	);
  
 }
 
